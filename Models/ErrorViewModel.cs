7ded135d108ff2de9e12247c775c7d1e6574a063
esraa <shaabanesraa02@gmail.com>
2024-12-04 13:49:20 +0200

    simple mvc dotnet app docarized

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace dontnet_dockarize.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
